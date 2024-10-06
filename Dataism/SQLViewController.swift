a662e6df2e8001f04c88ac7152101907cf07a283
CallihanB03 <calbertley2003@gmail.com>
2024-10-06 15:19:55 -0500

    Added table view controller for SQL Lessons

--- Dataism/SQLViewController.swift
+++ Dataism/SQLViewController.swift
@@ -7,12 +7,33 @@
 
 import UIKit
 
-class SQLViewController: UIViewController {
-
+class SQLViewController:
+    UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    
+    let sqlLessonsTableViewCellIdentifer = "SqlLessonTextCellIdentifier"
+    
+    @IBOutlet weak var sqlLessonsTableView: UITableView!
+    @IBOutlet weak var sqlTopicsLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        sqlLessonsTableView.delegate = self
+        sqlLessonsTableView.dataSource = self
+        sqlTopicsLabel.font = UIFont(name: standardFont, size: 20)
+    }
+    
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        sqlKeywords.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = sqlLessonsTableView.dequeueReusableCell(withIdentifier: sqlLessonsTableViewCellIdentifer, for: indexPath)
+        let row = indexPath.row
+            cell.textLabel?.text = sqlKeywords[row]
+            return cell
     }
     
 }
---
+++ Dataism/SqlLessons.swift
@@ -0,0 +1,27 @@
+//
+//  SqlLessons.swift
+//  Dataism
+//
+//  Created by Callihan Bertley on 10/6/24.
+//
+
+import Foundation
+
+
+let sqlKeywords = [ "SELECT",
+                  "INSERT",
+                  "UPDATE",
+                  "DELETE",
+                  "CREATE",
+                  "AS",
+                  "DISTINCT",
+                  "WHERE",
+                  "LIKE",
+                  "AND",
+                  "OR",
+                  "ORDER BY",
+                  "GROUP BY",
+                  "HAVING",
+                  "LIMIT",
+                  "SUBQUERIES"]
+
